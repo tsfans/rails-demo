98e8b8bb0f39c82e12aa746d923d091907b7b9d9
Swift Hu <swift.hu@oyorooms.com>
2018-05-27 23:37:34 +0800

    Finish ruby on rails official guide

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+    http_basic_authenticate_with name: "ruby", password: "rails", except: [:index, :show]
+
     def index
         @articles = Article.all
     end
